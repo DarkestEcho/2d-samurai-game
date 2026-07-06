19a0286f5b3afff4ee7bf4f77abdd76a0c114210
Dmitriy Samarin <Samarin99@gmail.com>
2026-07-06 13:40:51 +0300

    Update Player.gd

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,37 +3,58 @@ extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var flagState: FlagStateManager = $FlagStateManager
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-@onready var audio_player: AudioStreamPlayer2D = $AudioPlayer
-@onready var audio_player_2: AudioStreamPlayer2D = $AudioPlayer2
+@onready var jump_audio: AudioStreamPlayer2D = $JumpAudio
+@onready var land_audio: AudioStreamPlayer2D = $LandAudio
+@onready var attack_audio: AudioStreamPlayer2D = $AttackAudio
+@onready var footstep_audio: AudioStreamPlayer2D = $FootstepAudio
 
 const Flags = StateFlags.Flags;
 const FlagsPriority = StateFlags.FlagsPriority;
-const SPEED = 1000.0
+
+const SPEED = 800.0
 const SPEED_MOD = 30
-const JUMP_VELOCITY = -650.0
-const GRAVITY_MOD = 1.5
+const JUMP_VELOCITY = -750.0
+const GRAVITY_MOD = 2
 const COYOTE_TIME = 0.12 # in seconds
-const JUMP_BUFFER_TIME = 0.1 # in seconds
-const ATTACK_MOVE_TIME = 0.3 # in seconds
-const ATTACK_MOVE_TIME_AIR = 0.45 # in seconds
+const ATTACK_BUFFER_TIME = 0.25 # in seconds
+const JUMP_BUFFER_TIME = 0.15 # in seconds
+const ATTACK_COOLDOWN_TIME = 0.25 # in seconds
+
+const FOOTSTEPS_INTERVAL_MIN = 0.2;
+const FOOTSTEPS_INTERVAL_MAX = 0.5;
 
 const COLLISION_OFFSET_ATTACKING = 10;
 
 var coyote_timer: float = 0.0;
 var jump_buffer_timer: float = 0.0;
-var attack_move_timer: float = ATTACK_MOVE_TIME;
+var attack_buffer_timer: float = 0.0;
+var attack_cooldown_timer: float = 0.0;
+var footstep_timer: float = 0.0;
 
 var was_on_floor: bool = false;
 
 var current_animation = "";
 
-var Animations: Dictionary = {
+const Animations: Dictionary = {
 	Flags.Idle: "Idle",
 	Flags.Running: "Run",
 	Flags.Jumping: "Jump",
 	Flags.Falling: "Falling",
 	Flags.Attacking: "Attacking",
 }
+
+const AttackSounds = [
+	preload("uid://iac6vrdwnjhp"),
+	preload("uid://dtycusfuestqi"),
+]
+
+const FootstepsRunDirt = [
+	preload("uid://4wmbpnmpvy4f"),
+	preload("uid://bskdth5vmx0uk"),
+	preload("uid://bn278dk4cl53r"),
+	preload("uid://p4cayxmtlkoi"),
+	preload("uid://bfvolnq7jpd2k"),
+]
 
 
 func bin_count( value: int ) -> int:
@@ -44,17 +65,22 @@ func bin_count( value: int ) -> int:
 	return count;
 
 
-func _update_attack( delta: float ) -> void:
-	if flagState.Has( Flags.Attacking ) and attack_move_timer > 0.0:
-		attack_move_timer -= delta
-
-
 func _update_coyote( delta: float ) -> void:
 	if is_on_floor():
 		coyote_timer = COYOTE_TIME;
 		was_on_floor = true;
 	elif coyote_timer > 0.0:
 		coyote_timer -= delta;
+
+
+func _handle_attack() -> void:
+	if _can_attack():
+		flagState.Add( Flags.Attacking );
+		attack_buffer_timer = 0.0;
+		current_animation = "";
+		attack_audio.pitch_scale = randf_range( 0.8, 1.0 );
+		attack_audio.stream = AttackSounds[ randi_range( 0, AttackSounds.size() -1 ) ];
+		attack_audio.play();
 
 
 func _handle_jump() -> void:
@@ -65,17 +91,35 @@ func _handle_jump() -> void:
 		jump_buffer_timer = 0.0;
 		current_animation = "";
 		
-		audio_player.pitch_scale = randf_range( 0.8, 1.1 );
-		audio_player.stream = preload("uid://wijpk6ttmh3y");
-		audio_player.volume_db = 2.0;
-		audio_player.play();
+		jump_audio.pitch_scale = randf_range( 0.8, 1.1 );
+		jump_audio.play();
 
+
+func _handle_footsteps( delta: float ) -> void:
+	if flagState.HasNone( Flags.Running ) or flagState.HasAny( StateFlags.IN_AIR ):
+		footstep_timer = 0.0;
+		return;
+
+	footstep_timer -= delta;
+	if !( footstep_timer > 0.0 ):
+		_play_footsteps();
+		var speed_ratio: float = absf( velocity.x ) / SPEED;
+		footstep_timer = lerpf( FOOTSTEPS_INTERVAL_MIN, FOOTSTEPS_INTERVAL_MAX, speed_ratio );
+
+func _play_footsteps() -> void:
+	footstep_audio.pitch_scale = randf_range( 0.8, 1.0 );
+	footstep_audio.stream = FootstepsRunDirt[randi_range( 0, FootstepsRunDirt.size() -1 )];
+	footstep_audio.play();
+
+
+func _can_attack() -> bool:
+	return !( attack_cooldown_timer > 0.0 ) and attack_buffer_timer > 0.0 and flagState.HasNone( Flags.Attacking );
 
 func _can_jump() -> bool:
 	return ( is_on_floor() or coyote_timer > 0.0 );
 
-func _can_process_input() -> bool:
-	return flagState.HasNone( Flags.Attacking ) or attack_move_timer > 0.0;
+func _can_move() -> bool:
+	return flagState.HasNone( Flags.Attacking ) or flagState.HasAny( StateFlags.IN_AIR );
 
 
 func _get_animation() -> String:
@@ -112,10 +156,8 @@ var direction: int = 0;
 func _update_flags() -> void:
 	if is_on_floor():
 		if flagState.Has( Flags.Falling ):
-			audio_player_2.stream = preload("uid://ckjinmw5gqd5f");
-			audio_player_2.pitch_scale = randf_range( 0.8, 1.0 );
-			audio_player_2.volume_db = -7.0;
-			audio_player_2.play();
+			land_audio.pitch_scale = randf_range( 0.8, 1.0 );
+			land_audio.play();
 			flagState.Remove( Flags.Falling );
 
 		if velocity.x != 0 :
@@ -139,21 +181,20 @@ func _update_flags() -> void:
 		flagState.Remove( Flags.Idle );
 
 
-func _update_input( delta: float ) -> void:
-	if not _can_process_input():
-		return;
-		
+func _update_input( delta: float ) -> void:		
 	if Input.is_action_just_pressed("Attack"):
-		flagState.Add( Flags.Attacking );
-		attack_move_timer = ATTACK_MOVE_TIME;
-		if flagState.HasAny( StateFlags.IN_AIR ):
-			attack_move_timer = ATTACK_MOVE_TIME_AIR;
+		attack_buffer_timer = ATTACK_BUFFER_TIME;
+	elif attack_buffer_timer > 0.0:
+		attack_buffer_timer -= delta;
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump"):
 		jump_buffer_timer = JUMP_BUFFER_TIME;
 	elif jump_buffer_timer > 0.0:
 		jump_buffer_timer -= delta;
+
+	if not _can_move() or flagState.Has( Flags.Attacking ):
+		return;
 	
 	direction = 0;
 	if Input.is_action_pressed("Left") and direction != 1:
@@ -168,7 +209,7 @@ func _update_movement( delta: float ) -> void:
 		velocity += get_gravity() * GRAVITY_MOD * delta
 
 	velocity.x = 0.0;
-	if flagState.HasNone( Flags.Attacking ) or attack_move_timer > 0.0:
+	if _can_move():
 		velocity.x = direction * SPEED * SPEED_MOD * delta
 
 
@@ -181,6 +222,7 @@ func _on_animation_finished() -> void:
 		"Attacking":
 			flagState.Remove( Flags.Attacking );
 			current_animation = ""
+			attack_cooldown_timer = ATTACK_COOLDOWN_TIME;
 			pass
 
 
@@ -194,10 +236,16 @@ func _update_collisions() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	
+	if attack_cooldown_timer > 0.0:
+		attack_cooldown_timer -= delta;
+	
+
 	_update_input( delta );
 	_update_coyote( delta );
-	_update_attack( delta );
 	_handle_jump();
+	_handle_attack();
+	_handle_footsteps( delta );
 	_update_flags();
 	_update_movement( delta );
 	_update_animations();
